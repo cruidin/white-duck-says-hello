63a18ab4fa021b03dc03b0fe9d5b7d73349f1228
Patricia Melo <pelraton@gmail.com>
2021-07-05 15:05:09 +0100

    Add temporary images for testing javascript code for changing images in boxes and add array with file paths in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// color names need to be updated to match palette
+
+// Change Background Color On Click - HTML CSS and Javascript - https://www.youtube.com/watch?v=Wj9JpCuMV60
+
 var colors = ['pink', 'blue', 'green', 'yellow', 'lilac', 'orange', 'red'];
 var i = 0;
 document.querySelector('button').addEventListener('click', function() {
@@ -18,3 +22,25 @@ var logoImage = document.images["jsbutton"];
  function changeImageBack() {
    document.images['jsbutton'].src ="./assets/images/White-duck.png"
  }
+
+ // code foe changing boxes. Each box will be independently changed when corresponding
+ // button is clicked`
+
+let firstBoxImages = [
+  {
+    src: ".assets/images/bird-running-away.jpg",
+    height: "300px"
+  },
+  {
+    src: ".assets/images/birds-city.jpg",
+    height: "300px"
+  },
+  {
+    src: ".assets/images/birds-houses.jpg",
+    height: "300px"
+  },
+  {
+    src: ".assets/images/birds-park.jpg",
+    height: "300px"
+  },
+]
