9e2609decad420ca625f620dbe1bebecdb25860e
Patricia Melo <pelraton@gmail.com>
2021-07-11 16:14:03 +0100

    Finish adding functions to images in box 1, add modal basic structure to those images for more info about each image

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,46 +25,168 @@ var logoImage = document.images['jsbutton'];
    document.images['jsbutton'].src ='./assets/images/white-duck-outline.png'
  }
 
-//
+// Gallery in the first box
 
- var whiteDuckOutline = document.images['white-duck-outline'];
+// First image
+var whiteDuckOutline = document.images['white-duck-outline'];
 
- // to change image when mouse hovers
+// to change image when mouse hovers
   function changeImageOutline() {
     document.images['white-duck-outline'].src = './assets/images/white-duck-quack2.png';
     return true;
   }
 
- // to change back to the original image
+// to change back to the original image
   function changeImageOutlineBack() {
     document.images['white-duck-outline'].src ='./assets/images/white-duck-outline.png'
   }
 
+  // The Modal - a modal opens a window on the page
+  // We create a variable with the method getElementById to return the element
+  // that has the ID of myModal on index.html.
+  var modalOne = document.getElementById('outlineModal');
+
+  // We create another variable and use the method getElementById to get the button
+  // 'How to use this site' that opens the modal.
+  var modalButtonOne = document.getElementById('white-duck-outline');
+
+  // This variable gets the <span> element that closes the modal by using the method
+  // getElementsByClassName.
+  var spanOne = document.getElementsByClassName('closeOne')[0];
+
+  // This function will open the modal when the user clicks on 'How to use this site'.
+  // The style.display property is set to 'block' which means the element is rendered
+  // as a block-level. The onclick is being used here.
+  modalButtonOne.onclick = function() {
+    modalOne.style.display = 'block';
+  }
+
+  // This will close the modal when the user clicks on <span> (x). The onclick event
+  // is being used here.
+  spanOne.onclick = function() {
+    modalOne.style.display = 'none';
+  }
+
+  // The modal will close when user clicks anywhere outside of modal
+  window.onclick = function(event) {
+    if (event.target == modalOne) {
+      modalOne.style.display = 'none';
+    }
+  }
+
+// Second image
 var whiteDuckWizard = document.images['white-duck-wizard'];
 
-  // to change image when mouse hovers
-   function changeImageWizard() {
-     document.images['white-duck-wizard'].src = './assets/images/white-duck-wizard2.png';
-     return true;
-   }
+// to change image when mouse hovers
+  function changeImageWizard() {
+   document.images['white-duck-wizard'].src = './assets/images/white-duck-wizard2.png';
+   return true;
+  }
 
-  // to change back to the original image
-   function changeImageWizardBack() {
-     document.images['white-duck-wizard'].src ='./assets/images/white-duck-wizard.png'
-   }
+// to change back to the original image
+  function changeImageWizardBack() {
+   document.images['white-duck-wizard'].src ='./assets/images/white-duck-wizard.png'
+  }
 
-  var whiteDuckChill = document.images['white-duck-chill'];
+// Third image
+var whiteDuckChill = document.images['white-duck-chill'];
 
-     // to change image when mouse hovers
-      function changeImageChill() {
-        document.images['white-duck-chill'].src = './assets/images/white-duck-chill2.png';
-        return true;
-      }
+// to change image when mouse hovers
+  function changeImageChill() {
+    document.images['white-duck-chill'].src = './assets/images/white-duck-chill2.png';
+    return true;
+  }
 
-     // to change back to the original image
-      function changeImageChillBack() {
-        document.images['white-duck-chill'].src ='./assets/images/white-duck-chill.png'
-      }
+// to change back to the original image
+  function changeImageChillBack() {
+    document.images['white-duck-chill'].src ='./assets/images/white-duck-chill.png'
+  }
+
+// Fourth image
+var whiteDuckGlasses = document.images['white-duck-glasses'];
+
+// to change image when mouse hovers
+  function changeImageGlasses() {
+    document.images['white-duck-glasses'].src = './assets/images/white-duck-glasses2.png';
+    return true;
+  }
+
+// to change back to the original image
+  function changeImageGlassesBack() {
+    document.images['white-duck-glasses'].src ='./assets/images/white-duck-glasses.png'
+  }
+
+// Fifth image
+var whiteDuckApple = document.images['white-duck-apple'];
+
+// to change image when mouse hovers
+  function changeImageApple() {
+   document.images['white-duck-apple'].src = './assets/images/white-duck-apple2.png';
+   return true;
+  }
+
+// to change back to the original image
+  function changeImageAppleBack() {
+   document.images['white-duck-apple'].src ='./assets/images/white-duck-apple.png'
+  }
+
+// Sixth image
+var whiteDuckLeaves = document.images['white-duck-leaves'];
+
+// to change image when mouse hovers
+  function changeImageLeaves() {
+    document.images['white-duck-leaves'].src = './assets/images/white-duck-leaves2.png';
+    return true;
+  }
+
+// to change back to the original image
+  function changeImageLeavesBack() {
+    document.images['white-duck-leaves'].src ='./assets/images/white-duck-leaves.png'
+  }
+
+// Seventh image
+var whiteDuckLove = document.images['white-duck-love'];
+
+// to change image when mouse hovers
+  function changeImageLove() {
+    document.images['white-duck-love'].src = './assets/images/white-duck-love2.png';
+    return true;
+  }
+
+// to change back to the original image
+  function changeImageLoveBack() {
+    document.images['white-duck-love'].src ='./assets/images/white-duck-love.png'
+  }
+
+// Eight image
+var whiteDuckMonocle = document.images['white-duck-monocle'];
+
+// to change image when mouse hovers
+  function changeImageMonocle() {
+   document.images['white-duck-monocle'].src = './assets/images/white-duck-monocle2.png';
+   return true;
+  }
+
+// to change back to the original image
+  function changeImageMonocleBack() {
+   document.images['white-duck-monocle'].src ='./assets/images/white-duck-monocle.png'
+  }
+
+// Nineth image
+var whiteDuckChill = document.images['white-duck-chill'];
+
+// to change image when mouse hovers
+  function changeImageChill() {
+    document.images['white-duck-chill'].src = './assets/images/white-duck-chill2.png';
+    return true;
+  }
+
+// to change back to the original image
+  function changeImageChillBack() {
+    document.images['white-duck-chill'].src ='./assets/images/white-duck-chill.png'
+  }
+
+
 
  // code for changing boxes.
  // Array with images to be changed randomly
